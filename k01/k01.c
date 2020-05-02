a204d9bb213de3e38b3cd48c7812c33e28457706
ooki-takumi <bb35319012@ms.nagasaki-u.ac.jp>
2020-05-02 23:11:08 +0900

    試しに打っています

--- k01/k01.c
+++ k01/k01.c
@@ -4,13 +4,22 @@
 #include <math.h>
 
 extern double ave_online(double val,double ave)
-extern double var_online()
 
+{
+    return(((N-1)*ave/N)+(val/N));
+}
+extern double var_online()
+{
+    return(((N-1)*square_ave/N)+pow(val,2)/N)-pow((((N-1)*ave/N)+(val/N)),2);
+}
 int main(void)
 {
     double val;
     char fname[FILENAME_MAX];
     char buf[256];
+    double var,square_ave,unvar;
+    double ave =0;
+    int N=0;
     FILE* fp;
 
     printf("input the filename of sample:");
@@ -26,18 +35,25 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
+   N++;
+   var=var_online(val,ave,square_ave,N);
+   ave=ave_online(val,ave,N);
+   square_ave=ave_online(pow(val.2),square_ave,N);
 
 
 
     }
+
+    ubvar=N*var/(N-1);
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
+
+    printf("ave=%1f\n",stderr);
+    printf("var=%1f\n",var);
+    printf("unvar=%1f\n",unvar);
 
 
     return 0;
